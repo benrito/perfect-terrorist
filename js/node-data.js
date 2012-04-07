944a8f08da69f765b2db4a863dbfb498c60cb716
devin chalmers <devin.chalmers@gmail.com>
2012-04-07 17:58:02 -0300

    Update js/node-data.js

--- js/node-data.js
+++ js/node-data.js
@@ -1,7 +1,142 @@
 var node_data = {
-	"": {
+	berlin: {
 			
+		image_url: "Pakistan/Berlin Flight Changes.jpg",
+		video_url: "",
+		blurb: "Headley is arrested in Berline for smuggling drugs.  He is then approached by the DEA",
+		title: "Berlin Bust"
 	
-	
-	}
+	},
+	dea:{
+		image_url: "Dea/interrogation.jpg",
+		video_url: "",
+		blurb: "Headley was recruited by the DEA to be an informant.  This relationship was compromised when Headley became a L.E.T. member.",
+		title: "Recruited by the DEA"	
+	},
+	cartels:{
+		image_url: "",
+		video_url: "",
+		blurb: "",
+		title: ""	
+	},
+	pakistan:{
+		image_url: "Pakistan/bombing.jpg",
+		video_url: "",
+		blurb: "",
+		title: ""	
+	},
+	lashkar:{
+		image_url: "Pakistan/terrorists.jpg",
+		video_url: "",
+		blurb: "",
+		title: ""	
+	},
+	lahore:{
+		image_url: "",
+		video_url: "",
+		blurb: "",
+		title: ""	
+	},
+	embassy:{
+		image_url: "",
+		video_url: "",
+		blurb: "Fisa reports Headley to American Embassy",
+		title: "Embassy"	
+	},
+	taj_mahal:{
+		image_url: "killday/hotel monitorsw.jpeg",
+		video_url: "",
+		blurb: "Hotel Taj Mahal, location of one of the brutal attacks November 2008",
+		title: "Hotel Taj Mahal"	
+	},
+	mumbai:{
+		image_url: "",
+		video_url: "",
+		blurb: "",
+		title: ""	
+	},
+	warnings:{
+		image_url: "",
+		video_url: "",
+		blurb: "When Headley could have been stopped",
+		title: "Warnings"	
+	},
+	customs:{
+		image_url: "",
+		video_url: "",
+		blurb: "Headley is briefly detained at customs",
+		title: "Customs"	
+	},
+	arrests:{
+		image_url: "smuggling/False bottom suitcase.jpg",
+		video_url: "",
+		blurb: "Headley was arrested numerous times while running an international drug trade between the US and Pakistan",
+		title: "Arrests"	
+	},
+	nineEleven:{
+		image_url: "",
+		video_url: "",
+		blurb: "",
+		title: "9-11"	
+	},
+	questioning:{
+		image_url: "",
+		video_url: "",
+		blurb: "",
+		title: "Questioning"	
+	},
+	philly:{
+		image_url: "Terrorist Images/philly bar.jpg",
+		video_url: "",
+		blurb: "Philadelphia connection",
+		title: "Philadelphia connection"	
+	},
+	wife1:{
+		image_url: "",
+		video_url: "",
+		blurb: "",
+		title: "Shazia"	
+	},
+	wife2:{
+		image_url: "",
+		video_url: "",
+		blurb: "Headley met Canadian Portia in 1995.  They married in 2005 and divorced soon after Portia reported him to the authorities",
+		title: "Portia"	
+	},
+	wife3:{
+		image_url: "",
+		video_url: "",
+		blurb: "Married in 2007, Divorced soon thereafter",
+		title: "Fisa"	
+	},
+	mother:{
+		image_url: "",
+		video_url: "",
+		blurb: "Mother from Philadelphia",
+		title: ""	
+	},
+	iqbal:{
+		image_url: "",
+		video_url: "",
+		blurb: "",
+		title: ""	
+	},
+	mir:{
+		image_url: "",
+		video_url: "",
+		blurb: "",
+		title: ""	
+	},
+	rana:{
+		image_url: "",
+		video_url: "",
+		blurb: "",
+		title: ""	
+	},
+	newYork:{
+		image_url: "",
+		video_url: "",
+		blurb: "Headley moves to NYC in 1994 and meets a wife-to-be, Portia",
+		title: "New York City"	
+	},
 };
