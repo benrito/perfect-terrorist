1f348b6f5aa4f3f63638b8551ce07fc604aed954
Devin Chalmers <devin.chalmers@gmail.com>
2012-04-07 17:58:55 -0400

    Tweaks

---
+++ js/widget.js
@@ -0,0 +1,41 @@
+var popup;
+
+function createPopup(thisnode, title, imgsrc, videosrc, x, y){
+	if (popup) {
+	  removePopup();
+	}
+	
+  // if (!popup)
+  // {
+  	popup = $('<div id="popup"/>');
+  	var $title = $('<h2>' + title + '</h2>');
+
+  	popup.append("<a id='popup_link' href='" + videosrc + "'><img src='photos/" + imgsrc + "'></a>").append($title);
+  	popup.append("<a id='closebox' class='close' href='#'>X</a>");
+
+  	popup.css("top", y).css("left", x);
+
+  	$("#content").append(popup).animate({opacity:1},'slow');
+  	
+  	$('#closebox').click(function (e) {
+  		e.preventDefault();
+  		removePopup();
+  		//load video popup?
+  	}); 
+  // }
+	
+  // $("#content").append(popup).style("opacity", 1);
+  //  
+  // $("#popup h2").text(title);
+  // $("#popup a#popup_link").attr("href", videosrc);
+  // $("#popup img").attr("src", "photos/" + imgsrc);
+  // 
+  // popup.css("top", y).css("left", x);
+  
+}
+
+function removePopup(){
+	popup.animate({opacity:0},'slow');
+	popup.remove();
+	popup = null;
+}
