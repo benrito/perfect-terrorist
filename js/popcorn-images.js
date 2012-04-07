ecbbf2ca0c8b177710983b9d51a489a71540036f
Tom Giratikanon <giratikanon@gmail.com>
2012-04-07 16:47:43 -0400

    Updated popcorn image data, start segmenting popcorn scripts

---
+++ js/popcorn-images.js
@@ -0,0 +1,86 @@
+
+document.addEventListener("DOMContentLoaded", function () {
+    var pop = Popcorn("#popcorn");
+ 
+
+pop.image({
+id:'',
+start:'13',
+end:'14.5',
+src:'photos/Pakistan/Berlin Flight Changes.jpg',
+target:'webpage'});
+
+pop.image({
+id:'',
+start:'14.5',
+end:'16.5',
+src:'photos/Early Years Headley/ss9.jpg',
+target:'webpage'});
+
+pop.image({
+id:'',
+start:'16.5',
+end:'19.5',
+src:'photos/Drugs 2.jpg',
+target:'webpage'});
+
+// LOOK AT THIS 
+pop.image({
+id:'',
+start:'19.5',
+end:'23',
+src:'photos/cuffs.jpeg',
+target:'webpage'});
+
+pop.image({
+id:'',
+start:'23',
+end:'27',
+src:'photos/DEA/interrogation.jpg',
+target:'webpage'});
+
+pop.image({
+id:'',
+start:'33',
+end:'43',
+src:'photos/Early Years Headley/ss6.jpg',
+target:'webpage'});
+
+pop.image({
+id:'',
+start:'53',
+end:'56',
+src:'photos/mentor.jpg',
+target:'webpage'});
+
+/*
+pop.image({
+id:'',
+start:'',
+end:'24',
+src:'photos/ss8.jpeg',
+target:'webpage'});
+
+pop.image({
+id:'',
+start:'25',
+end:'27',
+src:'photos/ss9.jpeg',
+target:'webpage'});
+
+pop.image({
+id:'',
+start:'28',
+end:'30',
+src:'photos/ss10.jpeg',
+target:'webpage'});
+
+pop.image({
+id:'',
+start:'31',
+end:'33',
+src:'photos/ss11.jpeg',
+target:'webpage'});
+*/
+
+      }, false);
