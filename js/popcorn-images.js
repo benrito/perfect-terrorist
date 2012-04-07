dc323b4609bae9da6d425703b8c4b7f18aab74b6
Tom Giratikanon <giratikanon@gmail.com>
2012-04-07 17:05:24 -0400

    Add more pics

--- js/popcorn-images.js
+++ js/popcorn-images.js
@@ -2,27 +2,33 @@
 document.addEventListener("DOMContentLoaded", function () {
     var pop = Popcorn("#popcorn");
  
+pop.image({
+id:'',
+start:'5',
+end:'10',
+src:'photos/L.A dea clinic bust.jpg',
+target:'background'}); 
 
 pop.image({
 id:'',
 start:'13',
 end:'14.5',
 src:'photos/Pakistan/Berlin Flight Changes.jpg',
-target:'webpage'});
+target:'background'});
 
 pop.image({
 id:'',
 start:'14.5',
 end:'16.5',
 src:'photos/Early Years Headley/ss9.jpg',
-target:'webpage'});
+target:'background'});
 
 pop.image({
 id:'',
 start:'16.5',
 end:'19.5',
 src:'photos/Drugs 2.jpg',
-target:'webpage'});
+target:'background'});
 
 // LOOK AT THIS 
 pop.image({
@@ -30,57 +36,57 @@ id:'',
 start:'19.5',
 end:'23',
 src:'photos/cuffs.jpeg',
-target:'webpage'});
+target:'background'});
 
 pop.image({
 id:'',
 start:'23',
 end:'27',
 src:'photos/DEA/interrogation.jpg',
-target:'webpage'});
+target:'background'});
 
 pop.image({
 id:'',
 start:'33',
 end:'43',
 src:'photos/Early Years Headley/ss6.jpg',
-target:'webpage'});
+target:'background'});
 
 pop.image({
 id:'',
 start:'53',
 end:'56',
 src:'photos/mentor.jpg',
-target:'webpage'});
+target:'background'});
 
 /*
 pop.image({
 id:'',
 start:'',
-end:'24',
-src:'photos/ss8.jpeg',
-target:'webpage'});
+end:'',
+src:'photos/',
+target:''});
 
 pop.image({
 id:'',
 start:'25',
 end:'27',
 src:'photos/ss9.jpeg',
-target:'webpage'});
+target:'background'});
 
 pop.image({
 id:'',
 start:'28',
 end:'30',
 src:'photos/ss10.jpeg',
-target:'webpage'});
+target:'background'});
 
 pop.image({
 id:'',
 start:'31',
 end:'33',
 src:'photos/ss11.jpeg',
-target:'webpage'});
+target:'background'});
 */
 
       }, false);
