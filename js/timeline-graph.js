0373905db886f77c6aaee5c6a2feff557a490add
Devin Chalmers <devin.chalmers@gmail.com>
2012-04-07 17:23:08 -0400

    Add nodes and edges

--- js/timeline-graph.js
+++ js/timeline-graph.js
@@ -15,63 +15,385 @@ var colors =
     gray: "#8f8f8f"
   };
 
-var length = 20;
+var length = 168;
 
-var nodes = [
-  { id: 0,
-    x:  450,
-    y:  0,
-    size: 20,
-    in_time: 0,
-    out_time: 20,
-    color: colors.purple },
-  { id: 1,
-    x:  20,
-    y:  20,
-    size: 5,
-    in_time: 5,
-    out_time: 20,
-    source: 0,
-    color: colors.orange },
-  { id: 2,
-    x:  50,
-    y:  30,
-    size: 5,
-    in_time: 10,
-    out_time: 15,
-    source: 1,
-    color: colors.red },
-  { id: 3,
-    x:  40,
-    y:  70,
-    size: 5,
-    in_time: 10,
-    out_time: 20,
-    source: 1,
-    color: colors.blue }
-]
+var nodes = [{
+    "id": 0,
+    "x": 450,
+    "y": 0,
+    "size": 20,
+    "in_time": 0,
+    "out_time": Infinity,
+    "color": "#663399",
+    "strokeColor": "#5c2e8a"
+},
+{
+    "id": "dea",
+    "x": 289,
+    "y": 46,
+    "in_time": 10,
+    "out_time": Infinity,
+    "source": 0,
+    "size": 5,
+    "color": colors.gray
+},
+{
+    "id": "berlin",
+    "x": 183,
+    "y": 57,
+    "in_time": 15,
+    "out_time": Infinity,
+    "source": "dea",
+    "size": 5,
+    "color": colors.gray
+},
+{
+    "id": "cartels",
+    "x": 262,
+    "y": 114,
+    "in_time": 33,
+    "out_time": Infinity,
+    "source": "pakistan",
+    "size": 5,
+    "color": colors.gray
+},
+{
+    "id": "pakistan",
+    "x": 351,
+    "y": 146,
+    "in_time": 31,
+    "out_time": Infinity,
+    "source": "dea",
+    "size": 5,
+    "color": colors.gray
+},
+{
+    "id": "911",
+    "x": 584,
+    "y": 166,
+    "in_time": 36,
+    "out_time": Infinity,
+    "source": 0,
+    "size": 5,
+    "color": colors.gray
+},
+{
+    "id": "questioning",
+    "x": 598,
+    "y": 118,
+    "in_time": 40,
+    "out_time": Infinity,
+    "source": "911",
+    "size": 10,
+    "color": colors.red
+},
+{
+    "id": "mother",
+    "x": 712,
+    "y": 191,
+    "in_time": 65,
+    "out_time": Infinity,
+    "source": 0,
+    "size": 5,
+    "color": colors.gray
+},
+{
+    "id": "oxford",
+    "x": 727,
+    "y": 148,
+    "in_time": 70,
+    "out_time": Infinity,
+    "source": "mother",
+    "size": 5,
+    "color": colors.gray
+},
+{
+    "id": "fbi",
+    "x": 654,
+    "y": 157,
+    "in_time": 80,
+    "out_time": Infinity,
+    "source": "mother",
+    "size": 10,
+    "color": colors.red
+},
+{
+    "id": "lashkar",
+    "x": 257,
+    "y": 221,
+    "in_time": 58,
+    "out_time": Infinity,
+    "source": "pakistan",
+    "size": 5,
+    "color": colors.gray
+},
+{
+    "id": "lahore",
+    "x": 337,
+    "y": 218,
+    "in_time": 62,
+    "out_time": Infinity,
+    "source": "pakistan",
+    "size": 5,
+    "color": colors.gray
+},
+{
+    "id": "fiza",
+    "x": 232,
+    "y": 283,
+    "in_time": 109,
+    "out_time": Infinity,
+    "source": 0,
+    "size": 5,
+    "color": colors.gray
+},
+{
+    "id": "embassy",
+    "x": 334,
+    "y": 298,
+    "in_time": 112,
+    "out_time": Infinity,
+    "source": "fiza",
+    "size": 10,
+    "color": colors.red
+},
+{
+    "id": "taj_mahal",
+    "x": 276,
+    "y": 367,
+    "in_time": 134,
+    "out_time": Infinity,
+    "source": "fiza",
+    "size": 10,
+    "color": colors.red
+},
+{
+    "id": "mumbai",
+    "x": 370,
+    "y": 429,
+    "in_time": 135,
+    "out_time": Infinity,
+    "source": "mumbai",
+    "size": 5,
+    "color": colors.gray
+},
+{
+    "id": "arrest",
+    "x": 570,
+    "y": 309,
+    "in_time": 88,
+    "out_time": Infinity,
+    "source": "new_york",
+    "size": 5,
+    "color": colors.gray
+},
+{
+    "id": "new_york",
+    "x": 702,
+    "y": 289,
+    "in_time": 86,
+    "out_time": Infinity,
+    "source": 0,
+    "size": 5,
+    "color": colors.gray
+},
+{
+    "id": "second_wife",
+    "x": 652,
+    "y": 361,
+    "in_time": 89,
+    "out_time": Infinity,
+    "source": "new_york",
+    "size": 5,
+    "color": colors.gray
+},
+{
+    "id": "jttf",
+    "x": 766,
+    "y": 399,
+    "in_time": 97,
+    "out_time": Infinity,
+    "source": "second_wife",
+    "size": 5,
+    "color": colors.gray
+},
+{
+    "id": "customs",
+    "x": 562,
+    "y": 393,
+    "in_time": 149,
+    "out_time": Infinity,
+    "source": 0,
+    "size": 10,
+    "color": colors.red
+},
+{
+    "id": "warnings",
+    "x": 529,
+    "y": 431,
+    "in_time": 163,
+    "out_time": Infinity,
+    "source": "customs",
+    "size": 10,
+    "color": colors.red
+}];
 
 var edges = [
   {
-    source: 1,
-    target: 2,
-    value: 1
+    source: 0,
+    target: "dea",
+    weight: 1
   },
   {
-    source: 2,
-    target: 3,
-    value: 2
+    source: "dea",
+    target: "berlin",
+    weight: 1
+  },
+  {
+    source: "cartels",
+    target: 0,
+    weight: 1
+  },
+  {
+    source: "pakistan",
+    target: "cartels",
+    weight: 1
+  },
+  {
+    source: "dea",
+    target: "pakistan",
+    weight: 1
+  },
+  {
+    source: "pakistan",
+    target: 0,
+    weight: 1
+  },
+  {
+    source: "pakistan",
+    target: "lahore",
+    weight: 1
+  },
+  {
+    source: "lashkar",
+    target: "lahore",
+    weight: 1
+  },
+  {
+    source: "pakistan",
+    target: "lashkar",
+    weight: 1
+  },
+  {
+    source: "lashkar",
+    target: "embassy",
+    weight: 1
   },
   {
     source: 0,
-    target: 1,
-    value: 4
+    target: "fiza",
+    weight: 1
+  },
+  {
+    source: "fiza",
+    target: "embassy",
+    weight: 1
+  },
+  {
+    source: "embassy",
+    target: "taj_mahal",
+    weight: 1
+  },
+  {
+    source: "taj_mahal",
+    target: "mumbai",
+    weight: 1
+  },
+  {
+    source: "warnings",
+    target: "mumbai",
+    weight: 1
   },
   {
     source: 0,
-    target: 3,
-    value: 2
-  }
+    target: "questioning",
+    weight: 1
+  },
+  {
+    source: 0,
+    target: "911",
+    weight: 1
+  },
+  {
+    source: "arrest",
+    target: 0,
+    weight: 1
+  },
+  {
+    source: 0,
+    target: "customs",
+    weight: 1
+  },
+  {
+    source: "911",
+    target: "questioning",
+    weight: 1
+  },
+  {
+    source: "fbi",
+    target: "oxford",
+    weight: 1
+  },
+  {
+    source: "mother",
+    target: "oxford",
+    weight: 1
+  },
+  {
+    source: "mother",
+    target: "fbi",
+    weight: 1
+  },
+  {
+    source: 0,
+    target: "new_york",
+    weight: 1
+  },
+  {
+    source: "new_york",
+    target: "arrest",
+    weight: 1
+  },
+  {
+    source: "arrest",
+    target: "second_wife",
+    weight: 1
+  },
+  {
+    source: 0,
+    target: "second_wife",
+    weight: 1
+  },
+  {
+    source: "second_wife",
+    target: "jttf",
+    weight: 1
+  },
+  {
+    source: "fbi",
+    target: "warnings",
+    weight: 1
+  },
+  {
+    source: "customs",
+    target: "warnings",
+    weight: 1
+  },
+  {
+    source: 0,
+    target: "mother",
+    weight: 1
+  },
 ]
 
 
@@ -80,6 +402,7 @@ var setup_nodes = function() {
   // fixup nodes
   nodes.forEach(function(node){
     node.strokeColor = d3.interpolateRgb(node.color, "#000")(0.1);
+    node.x = (node.x - width / 2) * 1.25 + width / 2;
   });
   
   indexed_nodes = _.inject(nodes, function(idxed, node){
@@ -127,6 +450,9 @@ var node_source = function(node) {
 }
 
 var update = function(t) {
+  t = Math.floor(t);
+  if (currentTime && currentTime == t) return;
+  
   currentTime = t;
   
   var current_nodes = _.filter(nodes, function(node){
